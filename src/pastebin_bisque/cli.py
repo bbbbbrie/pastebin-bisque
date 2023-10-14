0f210cfdff3ec1500250e04638f878a9805c8492
Arshad <arshad.cwru@gmail.com>
2023-10-13 23:05:34 -0500

    remove commented line 57

--- src/pastebin_bisque/cli.py
+++ src/pastebin_bisque/cli.py
@@ -53,7 +53,6 @@ def is_good_response(resp):
     """
     content_type = resp.headers["Content-Type"].lower()
     return resp.status_code == 200 and content_type is not None
-    # and content_type.find('html') > -1)
 
 
 def log_error(e):
